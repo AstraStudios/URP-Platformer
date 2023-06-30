c02c6c9512f5d1aa3fceba3d37a070f6e4b9b5a8
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2023-06-30 12:42:34 -0500

    Added a pause menu

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MenuHandler : MonoBehaviour
+{
+    [SerializeField] GameObject pausePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void ReloadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenuScene", LoadSceneMode.Single);
+    }
+}
