4bed5a6c5091a9193ed6747b194757c0ab0e73ba
AstraStudios <98630492+AstraStudios@users.noreply.github.com>
2023-07-02 20:28:42 -0500

    Temp:For future

--- Assets/Scripts/QuickMainMenuFix.cs
+++ Assets/Scripts/QuickMainMenuFix.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class QuickMainMenuFix : MonoBehaviour
 {
     [SerializeField] GameObject mainPanel;
     [SerializeField] GameObject selectPanel;
+
+    public bool speedRunMode = false;
+    [SerializeField] TMP_Text speedrunCurr; // current speed run mode
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +22,11 @@ public class QuickMainMenuFix : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        speedrunCurr.text = speedRunMode.ToString();
+    }
+
+    public void SetSpeedRunMode()
+    {
+        speedRunMode = !speedRunMode;
     }
 }
